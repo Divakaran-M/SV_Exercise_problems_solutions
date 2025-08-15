b6df1dd3048ad97d52c16437aff80ea9146d7b84
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-15 15:49:54 +0530

    Create Problem06.sv

---
+++ Problem06.sv
@@ -0,0 +1,15 @@
+/* Problem 6
+Q: Replace all negative values in a array to zero */
+
+program non_Negative();
+  int array[];
+  initial begin
+    array = '{-1,0,12,-5,100,-9,7};
+    for(int i =0 ; i<$size(array)-1;i++)begin
+      if(array[i]<0)begin
+        array[i] = 0;
+      end
+    end
+    $display("Sanitized Codes:%p",array);
+  end
+endprogram
