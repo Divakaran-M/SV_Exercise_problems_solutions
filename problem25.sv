285c41f5aa0e8ce93c7ceb378c8be26cdf00b569
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:55:23 +0530

    Create problem25.sv

---
+++ problem25.sv
@@ -0,0 +1,22 @@
+/* Problem 25
+Q : Count total key value pairs in the associative array*/
+
+program assoc_array_exercises();
+  string assoc_arr[string];
+int total;
+initial begin
+assoc_arr["user1"] = "Admin";
+assoc_arr["user2"] = "Viewer";
+assoc_arr["user3"] = "Tester";
+assoc_arr["user4"] = "Admin";
+  foreach(assoc_arr[user])begin
+if(assoc_arr.exists(user))begin
+total += 1;
+end
+else begin
+$display("User not exists");
+end
+end 
+$display("Total Registered Users : %0d",total);
+end
+endprogram 
