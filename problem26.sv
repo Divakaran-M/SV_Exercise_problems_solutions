d9a54869a5bb5df07c2f0fccf9ee5d94ddf6dfc8
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:55:59 +0530

    Create problem26.sv

---
+++ problem26.sv
@@ -0,0 +1,13 @@
+/* Problem 26
+Q : Update the value of a key in associative array*/
+
+program assoc_array_exercises();
+  int assoc_arr[string];
+initial begin
+  assoc_arr["Core0"] = 70;
+  assoc_arr["Core1"] = 65;
+$display("%p",assoc_arr);
+assoc_arr["Core0"] = 75;
+$display("Core0 value updated to : %0d",assoc_arr["Core0"]);
+end
+endprogram 
