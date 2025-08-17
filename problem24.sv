a2ffc9e88d7273c83580daca8910847f9a0c74f0
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:54:43 +0530

    Create problem24.sv

---
+++ problem24.sv
@@ -0,0 +1,18 @@
+/* Problem 24
+Q : Use exists() function to check a key exist in associative array or not*/
+
+program assoc_array_exercises();
+  string assoc_arr[string];
+string ip;
+initial begin
+assoc_arr["192.168.1.1"] = "RouteA";
+assoc_arr["192.168.1.10"] = "RouteB";
+ip = "192.168.1.10";
+if(assoc_arr.exists(ip))begin
+$display("IP %s Found : TRUE",ip);
+end
+else begin
+$display("IP %s Found : False",ip);
+end
+end
+endprogram 
