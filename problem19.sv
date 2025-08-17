c1adb1682a71c73987c067b368d77418ea3e76bc
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:51:22 +0530

    Create problem19.sv

---
+++ problem19.sv
@@ -0,0 +1,15 @@
+/* Problem 19
+Q : deep copy of queue*/
+
+program dequeue();
+int unsigned queue1[$];
+  int unsigned queue2[$];
+initial begin
+  queue1 ='{11,12,13}; 
+  foreach(queue1[i])begin
+    queue2[i] = queue1[i];
+  end
+  $display("Queue 1 : %p",queue1);
+  $display("Queue 2 :%p", queue2);
+end
+endprogram 
