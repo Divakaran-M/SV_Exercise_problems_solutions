4e3ff83c292193c7498c87265f2cf487e8391d87
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:49:21 +0530

    Create problem16.sv

---
+++ problem16.sv
@@ -0,0 +1,17 @@
+/*Problem 16
+Q : Delete particular element in the queue(unknown index)*/
+
+program queue_max();
+int unsigned queue[$];
+int unsigned delete;
+initial begin
+queue = '{201, 202, 203, 202, 204};
+delete = 202;
+foreach(queue[i])begin
+if(delete == queue[i])begin
+queue.delete(i);
+end
+end
+$display("Updated Queue : %p",queue);
+end
+endprogram 
