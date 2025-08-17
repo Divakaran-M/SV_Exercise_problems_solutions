54b3f483809f528ad10aa61aaf4d7618d9c7ac89
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:48:31 +0530

    Create problem15.sv

---
+++ problem15.sv
@@ -0,0 +1,15 @@
+/* Problem 15
+Q : Insert element 999 at index 2 in the given queue*/
+
+program queue_max();
+int unsigned queue[$];
+initial begin
+queue = '{101, 102, 103, 104, 105};
+$display("Original queue : %p",queue);
+$display("Inserting 999 at index 2");
+queue.insert(2,999);
+$display("After inserted :%p ",queue);
+
+
+end
+endprogram 
