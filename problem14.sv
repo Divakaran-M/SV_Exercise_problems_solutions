4898daa796ddf513d43b1158e2d78c7d6ca8870a
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-17 21:47:43 +0530

    Create problem14.sv

---
+++ problem14.sv
@@ -0,0 +1,16 @@
+/* Problem 14
+Q : Find maximum number in the queue*/
+
+program queue_max();
+int unsigned queue[$];
+int unsigned max;
+initial begin
+queue = '{12, 35, 42, 18, 29, 50, 31};
+foreach(queue[i])begin
+if(max<queue[i])begin
+max = queue[i];
+end
+end
+$display("Maximum number is : %0d",max);
+end
+endprogram 
