b12531a5447ffd1c1148cd93719c0dc7b8c06979
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-19 18:48:27 +0530

    Update problem13.sv
    Used queue.sum() ,built in function

--- problem13.sv
+++ problem13.sv
@@ -3,12 +3,10 @@ Q: Add the elements in a queue */
 
 program queues();
 int unsigned queue[$];
-int unsigned total =0;
+int unsigned total;
 initial begin
 queue = '{120, 80, 200, 150, 50};
-for (int i = 0;i <=$size(queue);i++)begin
-total += queue[i];
-end
+ total = queue.sum();
   $display("Sum of elements in the Queue = %0d",total);
 end
 endprogram 
