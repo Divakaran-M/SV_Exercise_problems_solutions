0e905b405c1bf54875fa4c1a62b66e6d6e963562
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-21 07:36:46 +0530

    Update problem15.sv

--- problem15.sv
+++ problem15.sv
@@ -9,7 +9,9 @@ $display("Original queue : %p",queue);
 $display("Inserting 999 at index 2");
 queue.insert(2,999);
 $display("After inserted :%p ",queue);
-
-
 end
 endprogram 
+/* Output:
+Original queue : '{101, 102, 103, 104, 105}
+Inserting 999 at index 2
+After inserted :'{101, 102, 999, 103, 104, 105} */
