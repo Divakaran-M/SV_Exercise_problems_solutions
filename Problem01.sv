a839d562454c1c09a54b681f44da464deb757f49
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-21 06:38:17 +0530

    Update Problem01.sv

--- Problem01.sv
+++ Problem01.sv
@@ -4,7 +4,8 @@ Q: To find the sum of all the elements in a array using foreach loop.*/
 program sum_of_array();
   int unsigned total;
   initial begin
-    int unsigned temp_readings[] ={30,28,27,29,31,30,32,33,29,30};
+    int unsigned temp_readings[10];
+    temp_readings='{30,28,27,29,31,30,32,33,29,30};
   foreach(temp_readings[idx])
     begin
     total += temp_readings[idx];
@@ -12,3 +13,5 @@ program sum_of_array();
     $display("Time=%0t ns => Total Temperature = %0d",$time,total);
   end
 endprogram
+/* Output:
+Time=0 ns => Total Temperature = 299*/
