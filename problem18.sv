6bfc85655839c32d4035c875a04c11f4ac4bc3ec
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-21 07:44:00 +0530

    Update problem18.sv

--- problem18.sv
+++ problem18.sv
@@ -8,11 +8,18 @@ int unsigned i;
 initial begin
 //Enqueue 1,2,3
 queue = '{1,2,3};
+  $display("Before Dequeue:%p",queue);
 i = 0;
   while(queue.size()>0)begin
 dequeue = queue.pop_front();
 $display("Dequeued : %0d",dequeue);
 end
-
+  $display("After Dequeued:%p",queue);
 end
 endprogram 
+/* Output:
+Before Dequeue:'{1, 2, 3}
+Dequeued : 1
+Dequeued : 2
+Dequeued : 3
+After Dequeued:'{}*/
