d12d3234b345201d18067ebd514cc5bbc2e82478
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-21 07:02:22 +0530

    Update Problem06.sv

--- Problem06.sv
+++ Problem06.sv
@@ -2,7 +2,7 @@
 Q: Replace all negative values in a array to zero */
 
 program non_Negative();
-  int array[];
+  int array[7];
   initial begin
     array = '{-1,0,12,-5,100,-9,7};
     for(int i =0 ; i<$size(array)-1;i++)begin
@@ -13,3 +13,5 @@ program non_Negative();
     $display("Sanitized Codes:%p",array);
   end
 endprogram
+/* Output:
+Sanitized Codes:'{0, 0, 12, 0, 100, 0, 7}*/
