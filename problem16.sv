54526e8fc9a1683a290c99b0bc7d433fe812fa6f
Divakaran-M <divakaran.m.official@gmail.com>
2025-08-23 15:45:44 +0530

    Update problem16.sv

--- problem16.sv
+++ problem16.sv
@@ -1,20 +1,19 @@
 /*Problem 16
 Q : Delete particular element in the queue(unknown index)*/
 
-program queue_max();
+program queue_delete();
 int unsigned queue[$];
 int unsigned delete;
-int unsigned queue1[$];
 initial begin
 queue = '{201, 202, 203, 202, 204};
 delete = 202;
-for(idx = 0; idx < queue.size(); idx++) begin
+  for(int idx = 0; idx < queue.size(); idx++) begin
 if(queue[idx] == delete) begin
 queue.delete(idx);
 idx--;
 end
 end
-$display("Updated Queue : %p",queue1);
+$display("Updated Queue : %p",queue);
 end
 endprogram 
 /* Output:
